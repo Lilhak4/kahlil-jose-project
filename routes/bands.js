fb93f0c91a942d4ebb755e2eb47f6f3ecb993082
jose <jdellanza@gmail.com>
2018-07-16 17:20:53 +0200

    band dsplay and detail done

--- routes/bands.js
+++ routes/bands.js
@@ -6,10 +6,21 @@ router.get('/search', (req, res, next) => {
   const instrument = req.query.instrument_needed;
   Band.find({instrument_needed: instrument})
     .then(data => {
-      res.render('band-detail', {band: data});
+      res.render('display-bands', {bands: data});
     })
     .catch(err => {
       console.log(err);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+  const bandId = req.params.id;
+  Band.findById(bandId)
+    .then(band => {
+      res.render('band-details', {band: band});
+    })
+    .catch(error => {
+      console.log(error);
     });
 });
 
