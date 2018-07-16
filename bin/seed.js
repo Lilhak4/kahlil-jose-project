53e59119eb5dbd5865c6622c7e78ffaf24522bfb
jose <jdellanza@gmail.com>
2018-07-16 11:45:35 +0200

    fda

--- bin/seed.js
+++ bin/seed.js
@@ -9,28 +9,28 @@ mongoose.connect(`mongodb://localhost/${dbName}`);
 const bands = [
   {
     name: 'Manolo and the mofos',
-    intrument_needed: 'ngk',
+    instrument_needed: 'ngk',
     members: 'bgukfguk',
     user: 'kufbubu'
   },
 
   {
     name: 'Manolo and mamakanuleras',
-    intrument_needed: 'fhfdhfs',
+    instrument_needed: 'fhfdhfs',
     members: 'sgdndg',
     user: 'hsfnbsgn'
   },
 
   {
     name: 'Kahlil and the bros',
-    intrument_needed: 'nfsnsf',
+    instrument_needed: 'nfsnsf',
     members: 'nsfnfsn',
     user: 'mhfmdg'
   },
 
   {
     name: 'Jose in da hood',
-    intrument_needed: 'adbf',
+    instrument_needed: 'adbf',
     members: 'nfsng',
     user: 'advad'
   }
