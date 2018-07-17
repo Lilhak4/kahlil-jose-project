25347c578ffa4b3ba3043a278861f2b24606883f
jose <jdellanza@gmail.com>
2018-07-17 15:24:09 +0200

    band creator started

--- routes/bands.js
+++ routes/bands.js
@@ -2,6 +2,28 @@ const express = require('express');
 const router = express.Router();
 const Band = require('../models/band');
 
+router.get('/add', (req, res, next) => {
+  res.render('band-add');
+});
+
+router.post('/add', (req, res, next) => {
+  const data = {
+    name: req.body.name,
+    instrument_needed: req.body.instrument_needed,
+    members: req.body.members,
+    genre: req.body.genre,
+    owner: req.body.owner
+  };
+  const newBand = new Band(data);
+  newBand.save()
+    .then((bands) => {
+      res.redirect('/band'); //    ------>para que no se quede el ordenador aqui toda la vida
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
 router.get('/search', (req, res, next) => {
   const instrument = req.query.instrument_needed;
   Band.find({instrument_needed: instrument})
