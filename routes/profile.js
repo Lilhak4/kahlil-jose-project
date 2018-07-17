05e14f5960b5febe9e9aea53d37c71efd2511565
Kahlil Gersbach <kahlilgersbach@gmail.com>
2018-07-17 19:42:34 +0200

    profile page trying to include band that user owns as a detail

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Band = require('../models/band');
+const User = require('../models/user');
 
 router.get('/', function (req, res, next) {
   res.render('profile');
 });
+
+// if (req.body.owner == req.body.username) { ***trying to include current band that the user is a owner***
+
+// }
 
 module.exports = router;
