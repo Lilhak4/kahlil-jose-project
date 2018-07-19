0cec3c4e9cb83756d0f4695f40e68417990744c4
jose <jdellanza@gmail.com>
2018-07-19 20:26:32 +0200

    applicants view running

--- routes/apply.js
+++ routes/apply.js
@@ -6,9 +6,37 @@ router.get('/:id', (req, res, next) => {
   const bandId = req.params.id;
   Band.findById(bandId)
     .populate('applicants')
-    .then((data) => {
-      const applicants = data.applicants;
-      res.render('band-apply', {applicants});
+    .then((result) => {
+      const data = {
+        band: result,
+        applicants: result.applicants
+      };
+      res.render('band-apply', data);
+    })
+    .catch(next);
+});
+
+router.post('/:id/accept', (req, res, next) => {
+  const bandId = req.params.id;
+  const applicant = req.body.applyId;
+  const update = { $unset: { applicants: applicant } };
+  const update2 = { $push: { members: applicant } };
+  Band.findByIdAndUpdate(bandId, update, update2)
+    .then(result => {
+    //   req.flash('apply-message', 'Applicant added');
+      res.redirect('/band/' + bandId);
+    })
+    .catch(next);
+});
+
+router.post('/:id/reject', (req, res, next) => {
+  const bandId = req.params.id;
+  const applicant = req.body.applyId;
+  const update = { $unset: { applicants: applicant } };
+  Band.update(bandId, update)
+    .then(result => {
+    //   req.flash('apply-message', 'Applicant removed');
+      res.redirect('/band/' + bandId);
     })
     .catch(next);
 });
