dab5c1973174cde8901a01fda483ef73dcc59bc5
jose <jdellanza@gmail.com>
2018-07-19 21:48:07 +0200

    add memeber remove instrmunet needed

--- routes/apply.js
+++ routes/apply.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Band = require('../models/band');
+const User = require('../models/user');
 
 router.get('/:id', (req, res, next) => {
   const bandId = req.params.id;
@@ -19,14 +20,19 @@ router.get('/:id', (req, res, next) => {
 router.post('/:id/accept', (req, res, next) => {
   const bandId = req.params.id;
   const applicant = req.body.applyId;
+  const applicantInstrument = req.body.applyInstrument;
   const update = { $push: { member: applicant } };
   const update2 = { $pull: { applicants: applicant } };
+  const update3 = { $pull: { instrument_needed: applicantInstrument } };
   Band.findByIdAndUpdate(bandId, update)
     .then(result => {
     //   req.flash('apply-message', 'Applicant added');
       return Band.findByIdAndUpdate(bandId, update2);
     })
     .then(result2 => {
+      return Band.findByIdAndUpdate(bandId, update3);
+    })
+    .then(result3 => {
       res.redirect('/band/' + bandId);
     })
     .catch(next);
