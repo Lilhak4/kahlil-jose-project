b01bebcf016ab0baa421b8573091d3953ec10ce2
jose <jdellanza@gmail.com>
2018-07-20 02:17:10 +0200

    redoing styles

--- bin/seed.js
+++ bin/seed.js
@@ -1,56 +1,56 @@
-'use strict';
+// 'use strict';
 
-const mongoose = require('mongoose');
-const Band = require('../models/band');
+// const mongoose = require('mongoose');
+// const Band = require('../models/band');
 
-const dbName = 'brand-title';
-mongoose.connect(`mongodb://localhost/${dbName}`);
+// const dbName = 'brand-title';
+// mongoose.connect(`mongodb://localhost/${dbName}`);
 
-const bands = [
-  {
-    name: 'Mc Kahlil',
-    instrument_needed: ['Singer', 'Bass'],
-    members: 'bgukfguk',
-    owner: 'Kahlil',
-    genre: 'Hip-hop'
-  },
+// const bands = [
+//   {
+//     name: 'Mc Kahlil',
+//     instrument_needed: ['Singer', 'Bass'],
+//     members: 'bgukfguk',
+//     owner: 'Kahlil',
+//     genre: 'Hip-hop'
+//   },
 
-  {
-    name: 'Manolo and mamakanuleras',
-    instrument_needed: ['Guitar', 'Singer', 'Bass'],
-    members: 'sgdndg',
-    owner: 'Manolo',
-    genre: 'Punk-rock'
-  },
+//   {
+//     name: 'Manolo and mamakanuleras',
+//     instrument_needed: ['Guitar', 'Singer', 'Bass'],
+//     members: 'sgdndg',
+//     owner: 'Manolo',
+//     genre: 'Punk-rock'
+//   },
 
-  {
-    name: 'Coucou band',
-    instrument_needed: ['Guitar', 'Bass', 'Drums'],
-    members: 'nsfnfsn',
-    owner: 'Julien',
-    genre: 'Jazz'
-  },
-  {
-    name: 'Jose in da hood',
-    instrument_needed: ['Guitar', 'Singer', 'Drums', 'Bass'],
-    members: 'nfsng',
-    owner: 'Jose',
-    genre: 'Metal'
-  }
-  // {
+//   {
+//     name: 'Coucou band',
+//     instrument_needed: ['Guitar', 'Bass', 'Drums'],
+//     members: 'nsfnfsn',
+//     owner: 'Julien',
+//     genre: 'Jazz'
+//   },
+//   {
+//     name: 'Jose in da hood',
+//     instrument_needed: ['Guitar', 'Singer', 'Drums', 'Bass'],
+//     members: 'nfsng',
+//     owner: 'Jose',
+//     genre: 'Metal'
+//   }
+//   // {
 
-  //   name: 'Waca-band',
-  //   instrument_needed: [' Bass', ' Singer', ' Drums'],
-  //   members: 'nfsng',
-  //   owner: 'advad',
-  //   genre: 'Tropical-pachangoso'
-  // }
-];
-Band.create(bands)
-  .then(() => {
-    console.log(`Created ${bands.length} bands`);
-    mongoose.connection.close();
-  })
-  .catch((err) => {
-    throw (err);
-  });
+//   //   name: 'Waca-band',
+//   //   instrument_needed: [' Bass', ' Singer', ' Drums'],
+//   //   members: 'nfsng',
+//   //   owner: 'advad',
+//   //   genre: 'Tropical-pachangoso'
+//   // }
+// ];
+// Band.create(bands)
+//   .then(() => {
+//     console.log(`Created ${bands.length} bands`);
+//     mongoose.connection.close();
+//   })
+//   .catch((err) => {
+//     throw (err);
+//   });
